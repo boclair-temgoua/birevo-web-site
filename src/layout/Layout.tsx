c9e855a096b6cf7e3d1ad75192b700e2f731abf5
Boclair Temgoua <temgoua2000@gmail.com>
2022-09-15 17:55:24 +0200

    first commit

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Head from 'next/head';
+
+export type LayoutProps = {
+  title: string;
+  desc: string;
+  hasClass: string;
+  classOpt: string;
+  children: JSX.Element,
+}
+
+const Layout = ({ children, title, desc, hasClass, classOpt }: LayoutProps) => {
+  return (
+    <div className={`main-wrapper ${hasClass ? 'overflow-hidden' : null} ${classOpt}`}>
+      <Head>
+        <title>
+          {title
+            ? `${process.env.NEXT_PUBLIC_NAME} | ${title}`
+            : `${process.env.NEXT_PUBLIC_NAME} - Create and manage coupons or vouchers from a single platform`}
+        </title>
+        {desc && <meta name="description" content={desc} />}
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/game-solution/index.js
@@ -0,0 +1,29 @@
+import GameCounter from '@components/counter/GameCounter'
+import GameFaq from '@components/faq/GameFaq'
+import GameFeatures from '@components/features/GameFeaures'
+import HeroTwelve from '@components/hero-section/HeroTwelve'
+import GamePrice from '@components/pricing/GamePrice'
+import GameReview from '@components/review/GameReview'
+import GameServer from '@components/server/GameServer'
+import GameServices from '@components/services/GameServices'
+import GameFooter from '@layout/Footer/GameFooter'
+import Navbar from '@layout/Header/Navbar'
+import Layout from '@layout/Layout'
+import React from 'react'
+
+export default function index() {
+  return (
+    <Layout title='Game Server' classOpt='bg-black'>
+      <Navbar navDark />
+      <HeroTwelve />
+      <GameServer />
+      <GameServices />
+      <GameFeatures />
+      <GameCounter />
+      <GamePrice />
+      <GameFaq />
+      <GameReview />
+      <GameFooter />
+    </Layout>
+  )
+}
